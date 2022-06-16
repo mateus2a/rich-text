39568032462e3636b800de4782353c7abdead7a3
Mateus <mtauws@gmail.com>
2022-06-16 13:01:37 -0300

    chore: create data of buttons

---
+++ src/data/buttons.tsx
@@ -0,0 +1,13 @@
+export const buttons = [
+  { dataElement: "bold", class: "fa fa-bold" },
+  { dataElement: "italic", class: "fa fa-italic" },
+  { dataElement: "underline", class: "fa fa-underline" },
+  { dataElement: "insertUnorderedList", class: "fa fa-ul" },
+  { dataElement: "insertOrderedList", class: "fa fa-ol" },
+  { dataElement: "createLink", class: "fa fa-link" },
+  { dataElement: "justifyLeft", class: "fa fa-align-left" },
+  { dataElement: "justifyCenter", class: "fa fa-align-center" },
+  { dataElement: "justifyRight", class: "fa fa-align-right" },
+  { dataElement: "justifyFull", class: "fa fa-align-justify" },
+  { dataElement: "insertImage", class: "fa fa-image" },
+];
