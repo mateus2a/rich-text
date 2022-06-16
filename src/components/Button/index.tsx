e1ae3cc534169539770e35d558228446f150b053
Mateus <mtauws@gmail.com>
2022-06-16 14:18:02 -0300

    chore: add styles to button component

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -3,11 +3,18 @@ import React from "react";
 
 import { buttons } from "../../data/buttons";
 
+import "./styles.css";
+
 function Button() {
   return (
     <div className="text-editor-header">
       {buttons.map((e) => (
-        <button type="button" key={e.dataElement} data-element={e.dataElement}>
+        <button
+          type="button"
+          className="btn"
+          key={e.dataElement}
+          data-element={e.dataElement}
+        >
           <FontAwesomeIcon icon={e.icon} />
         </button>
       ))}
