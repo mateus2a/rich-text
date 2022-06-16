0b2e976c6ca50faafcde3213c81b5830700169c9
Mateus <mtauws@gmail.com>
2022-06-16 14:52:58 -0300

    some features

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,19 @@ import EditTable from "./pages/EditTable";
 import "./App.css";
 
 function App() {
+  // const elements = document.querySelectorAll(".btn");
+
+  // elements.forEach((element) => {
+  //   element.addEventListener("click", () => {
+  //     const command = element.dataset.element;
+  //     console.log(element);
+  //     if (command === "createLink" || command === "insertImage") {
+  //       document.execCommand(command, false, url);
+  //     }
+  //     document.execCommand(command, false);
+  //   });
+  // });
+
   return (
     <div className="main-content">
       <Button />
